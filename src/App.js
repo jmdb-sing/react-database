e88f6486c2c014bbce97cfa6aa06e94719af8121
jmdb-sing <147319541+jmdb-sing@users.noreply.github.com>
2024-02-10 15:33:09 +0800

    Update App.js
    created a big div element for classes later on

--- src/App.js
+++ src/App.js
@@ -2,32 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <><div>
-      <head>
-        GGG TEAM
-      </head>
-    </div>
-    
-    <body>
-
-    <div class="flex-container">
+    <div>
       <div>
-
-        <img src='https://i.imgur.com/uV2l7zR.gif' alt="My Favorite Vegetable"></img>
-
+        <head>
+          GGG TEAM
+        </head>
       </div>
-      <div class="info">
-        <h1>Name: Drake</h1>
-        <h3>Scientific Name: Aubrey Drake Graham</h3>
-        <h3>Type of G: GO</h3>
+      
+      <body>
+  
+      <div class="flex-container">
+        <div>
+  
+          <img src='https://i.imgur.com/uV2l7zR.gif' alt="My Favorite Vegetable"></img>
+  
+        </div>
+        <div class="info">
+          <h1>Name: Drake</h1>
+          <h3>Scientific Name: Aubrey Drake Graham</h3>
+          <h3>Type of G: GO</h3>
 
-        <h1 id='pros'>PROS: </h1>
-        <h1 id='cons'>CONS: </h1>
+          <h1 id='pros'>PROS: </h1>
+          <h1 id='cons'>CONS: </h1>
+        </div>
       </div>
-    </div>
 
-      </body>
-      </>
+        </body>
+      </div>
   );
 }
 
